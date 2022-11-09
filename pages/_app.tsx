873da1e1a154a734b6eb738fa7450c8bba76e224
Fernando <fernando.sabaiz@gmail.com>
2022-11-09 10:36:58 -0600

    redux connects to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
+// Packages
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+// Global Styles
+import '../styles/globals.css'
+
+// Wrapper Redux
+import { wrapper } from '../redux/store'
+
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default wrapper.withRedux(App);
