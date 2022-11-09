8920d948611264e4e64a1b3e337a88ef1a27b84d
Fernando <fernando.sabaiz@gmail.com>
2022-11-09 10:39:43 -0600

    login and user pages are created

---
+++ pages/login/index.tsx
@@ -0,0 +1,10 @@
+// Packages
+
+import React from 'react'
+import LoginContainer from '../../containers/LoginContainer'
+
+const Login: React.FC = () => {
+    return ( <LoginContainer />)
+}
+
+export default Login
---
+++ pages/users/index.tsx
@@ -0,0 +1,18 @@
+// Packages
+import React from 'react'
+
+// Components
+import Layout from '../../components/Layout'
+
+// Container
+import UserContainer from '../../containers/UsersContainer'
+
+const Users: React.FC = () => {
+    return (
+        <Layout>
+            <UserContainer />
+        </Layout>
+    )
+}
+
+export default Users
