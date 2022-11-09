09696f27c4223d5bc736732baf9633be2c8cbed0
Fernando <fernando.sabaiz@gmail.com>
2022-11-09 10:27:45 -0600

    Config project

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        permanent: false,
+        destination: "/login",
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'reqres.in',
+        port: '',
+        pathname: '/img/faces/**',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
